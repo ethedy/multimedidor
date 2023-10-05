b827a9ce15ad8695bbcf367eab016d4551be5bd0
Tano <jesustano.jt@gmail.com>
2023-10-04 21:09:52 -0300

    prueba

---
+++ src/PROYECTO 2023/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+
+void loop() {
+  printf ("Hello world \n");
+}
+
